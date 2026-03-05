d354c74ae36e241bc2c9e1234f918f9a7d9b8f4f
George Karacsonyi <gkaracso@gmail.com>
2026-03-05 22:26:52 +0000

    add site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://dev.georgejosef.com",
+  base: "/sentinel-systems/client/",
   vite: {
     plugins: [tailwindcss()]
   }
